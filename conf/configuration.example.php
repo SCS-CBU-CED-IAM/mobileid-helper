76a163a18a3595aeb9f68b6452f729f13b58604f
Freddy Kaiser <kaiser.freddy@gmail.com>
2013-01-11 16:30:43 +0100

    Cleanups refs #17

--- conf/configuration.example.php
+++ conf/configuration.example.php
@@ -2,30 +2,29 @@
 class mobileIdConfig {
 
 	/* Client certificate configuration */
-	public $cert_ca   = "/data/webdev/www/dev.swisscom.ch/conf/swisscom-ca.crt";		// Bag file with the server/client issuing and root certifiates
-	public $cert_file = "/data/webdev/www/dev.swisscom.ch/conf/mycert.crt";				// The certificate that is allowed to access the service
-	public $cert_key  = "/data/webdev/www/dev.swisscom.ch/conf/mycert.key";				// The related key of the certificate
+	public $cert_ca   = "/www/conf/swisscom-ca.crt";        // Bag file with the server/client issuing and root certifiates
+	public $cert_file = "/www/conf/mycert.crt";             // The certificate that is allowed to access the service
+	public $cert_key  = "/www/conf/mycert.key";             // The related key of the certificate
 	
 	/* AP configuration */
-	public $ap_id  = "http://iam.swisscom.ch";											// AP UserID provided by Swisscom
-	public $ap_pwd = "disabled";														// AP Password must be present but is not validated
+	public $ap_id  = "<AP UserID>";                         // AP UserID provided by Swisscom
+	public $ap_pwd = "disabled";                            // AP Password must be present but is not validated
 	
 	/* OCSP configuration */
-	public $ocsp_cert = "/data/webdev/www/dev.swisscom.ch/conf/swisscom-ocsp.crt";		// OCSP information of the signers certificate
-	//public $ocsp_url  = "http://ocsp.swissdigicert.ch/sdcs-rubin2";					// Not mandatory
+	public $ocsp_cert = "/www/conf/swisscom-ocsp.crt";      // OCSP information of the signers certificate
 	
 	/* Soap configuration */
 	public $ws_url    = "https://soap.mobileid.swisscom.com/soap/services/MSS_SignaturePort";
 	public $ws_action = "#MSS_Signature";
 
 	/* Set the timeout for the request */
-	//public $TimeOutWSRequest  = 90;													// Not mandatory
-	//public $TimeOutMIDRequest = 80;													// Not mandatory
+	//public $TimeOutWSRequest  = 90;                       // Optional, to set the timeout of the web service call
+	//public $TimeOutMIDRequest = 80;                       // Optional, to set the timeout of the mobile id call
 
-	/* Request messages  */
-	public $mid_msg_de = "Erlauben Sie das testen Ihrer Mobile ID ?";
-	public $mid_msg_en = "Allow testing of your Mobile ID?";
-	public $mid_msg_fr = "Authoriser le test de la Mobile ID ?";
-	public $mid_msg_it = "Allow testing of your Mobile ID?";
+	/* Request messages, be sure to have your service prefixed  */
+    public $mid_msg_de = "serviceprovider.com: Erlauben Sie das testen Ihrer Mobile ID?";
+    public $mid_msg_en = "serviceprovider.com: Allow testing of your Mobile ID?";
+    public $mid_msg_fr = "serviceprovider.com: Authoriser le test de la Mobile ID?";
+    public $mid_msg_it = "serviceprovider.com: Permetta le prove della vostra Mobile ID?";
 }
 ?>
