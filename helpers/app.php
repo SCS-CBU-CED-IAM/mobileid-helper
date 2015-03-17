0dc7096e8323c02a83f1aad0726a6dcaf6c89f17
Freddy Kaiser <kaiser.freddy@gmail.com>
2015-03-17 18:28:14 +0100

    From GPL to Apache

--- helpers/app.php
+++ helpers/app.php
@@ -3,8 +3,8 @@
  * @version     1.0.0
  * @package     mobileid-helper
  * @copyright   Copyright (C) 2012. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.md
- * @author      Swisscom (Schweiz AG)
+ * @license     Licensed under the Apache License, Version 2.0 or later; see LICENSE.md
+ * @author      Swisscom (Schweiz) AG
  */
  
 /* Requirements */
