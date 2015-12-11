0029f2968dc17ef996b973d43060a9a4a953283b
Freddy Kaiser <kaiser.freddy@gmail.com>
2015-12-11 17:31:54 +0100

    Cleanups and translations refs #71, #72

--- conf/configuration.example.php
+++ conf/configuration.example.php
@@ -9,23 +9,29 @@
     
 class mobileIdConfig {
 
-	/* AP configuration */
-	public $ap_id = "<AP UserID>";             					// AP ID provided by Swisscom
-	public $ap_pwd = "disabled";                            	// AP Password provided by Swisscom
-	public $ap_cert = "/www/conf/mycertandkey.crt";				// Certificate/key that is allowed to access the service
+	/* AP configuration */	
+	// AP ID provided by Swisscom
+	public $ap_id = "<AP UserID>";
+	// AP Password provided by Swisscom
+ 	public $ap_pwd = "disabled";
+	// Certificate/key that is allowed to access the service
+	public $ap_cert = "conf/mycertandkey.crt";
 
 	/* Client certificate configuration */
-	public $ca_ssl = "/www/conf/mobileid-ca-ssl.crt";			// Location of Certificate Authority file which should be used to authenticate the identity of the remote peer
-	public $ca_mid = "/www/conf/mobileid-ca-signature.crt";		// Location of CA file which should be used during verifications
+	// Location of Certificate Authority file which should be used to authenticate the identity of the remote peer
+	public $ca_ssl = "conf/mobileid-ca-ssl.crt";
+	// Location of CA file which should be used during verifications
+	public $ca_mid = "conf/mobileid-ca-signature.crt";
 
 	/* Message provider */
-	public $mid_msg_service = "serviceprovider.com";        	// Defines the prefix for the request messages
-
+	// Defines the prefix for the request messages
+	public $mid_msg_service = "serviceprovider.com";      
+	  
 	/* Request messages. Those will be prefixed with the Message provider */
-    public $mid_msg_de = "Erlauben Sie das testen Ihrer Mobile ID? (#TRANSID#)";
-    public $mid_msg_en = "Allow testing of your Mobile ID? (#TRANSID#)";
-    public $mid_msg_fr = "Autoriser le test de la Mobile ID? (#TRANSID#)";
-    public $mid_msg_it = "Permetta le prove della vostra Mobile ID? (#TRANSID#)";
+	public $mid_msg_de = "Erlauben Sie das testen Ihrer Mobile ID? (#TRANSID#)";
+	public $mid_msg_en = "Allow testing of your Mobile ID? (#TRANSID#)";
+	public $mid_msg_fr = "Autoriser le test de la Mobile ID? (#TRANSID#)";
+	public $mid_msg_it = "Permetta le prove della vostra Mobile ID? (#TRANSID#)";
 	
 	/* Allow message edition */	
 	public $mid_msg_allowedit = false;
